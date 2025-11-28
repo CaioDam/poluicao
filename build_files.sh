5abc683cb476dc4e498c51e8f91ff9b470c8e048
CaioDam <161773985+CaioDam@users.noreply.github.com>
2025-11-28 16:58:57 -0300

    Create build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+
+echo "Migrando banco de dados..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python3 manage.py collectstatic --noinput
